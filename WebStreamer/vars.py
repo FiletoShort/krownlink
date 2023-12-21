9ccd59cecbbd41bf5698ffc7ebb6c97b9f9eb663
FiletoShort <148169673+FiletoShort@users.noreply.github.com>
2023-12-22 00:12:39 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -8,13 +8,13 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(environ.get("API_ID","13415474"))
-    API_HASH = str(environ.get("API_HASH","01bb828c0429beeabd6e9e841d026231"))
-    BOT_TOKEN = str(environ.get("BOT_TOKEN", "6666952538:AAGb8Quqe0N0Ps8GDoPB5XYjSMjUIzfWQ28"))
-    SHORTNER_API = str(environ.get("SHORTNER_API", "4a03e5ec6a4a682d13fcd6fab6d649ef19c23cea")) 
-    SHORTENR_URL = str(environ.get("SHORTENR_URL", "mplaylink.com")) 
-    AUTH_CHANNEL = str(environ.get("AUTH_CHANNEL", "TamilMov_chat")) 
-    CUSTOM_FILE_CAPTION = str(environ.get("CUSTOM_FILE_CAPTION","** {file_name} \n\n⚡️ 𝐅𝐚𝐬𝐭 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐋𝐢𝐧𝐤 👇 \n {short_link} \n\n💢Join : @{main_chat} **")) 
+    API_ID = int(environ.get("API_ID","24652824"))
+    API_HASH = str(environ.get("API_HASH","eaca6d81c81f01149442cfeee6dbb4be"))
+    BOT_TOKEN = str(environ.get("BOT_TOKEN", "6708407834:AAEykhoR-L3fJ80TJP7XiHwY5nvzdKbNcGw"))
+    SHORTNER_API = str(environ.get("SHORTNER_API", "15c728d0af696e2c4751e0d3a965a0652b393ea7")) 
+    SHORTENR_URL = str(environ.get("SHORTENR_URL", "tnshort.net")) 
+    AUTH_CHANNEL = str(environ.get("AUTH_CHANNEL", "SAM_DUB_LEZHa")) 
+    CUSTOM_FILE_CAPTION = str(environ.get("CUSTOM_FILE_CAPTION","** {file_name} \n\n ➠Fast Download Link : \n {short_link} **")) 
     SLEEP_THRESHOLD = int(environ.get("SLEEP_THRESHOLD", "60"))  # 1 minte
     WORKERS = int(environ.get("WORKERS", "6"))  # 6 workers = 6 commands at once
     BIN_CHANNEL = int(environ.get("BIN_CHANNEL", "-1002082897617"))  # you NEED to use a CHANNEL when you're using MULTI_CLIENT
